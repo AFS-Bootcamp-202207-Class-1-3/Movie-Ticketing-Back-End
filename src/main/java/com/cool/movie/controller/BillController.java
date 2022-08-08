b08ac10557a4cd2fd1b4b3ebe631676bd1a05d82
huangjo13 <19259907@qq.com>
2022-08-08 15:30:23 +0800

    feat: add getBill api

---
+++ src/main/java/com/cool/movie/controller/BillController.java
@@ -0,0 +1,22 @@
+package com.cool.movie.controller;
+
+import com.cool.movie.dto.BillResponse;
+import com.cool.movie.service.BillService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("bills")
+public class BillController {
+    @Resource
+    BillService billService;
+
+    @GetMapping("/{orderId}")
+    public BillResponse getBillByOrderId(@PathVariable String orderId) {
+        return billService.getBill(orderId);
+    }
+}
