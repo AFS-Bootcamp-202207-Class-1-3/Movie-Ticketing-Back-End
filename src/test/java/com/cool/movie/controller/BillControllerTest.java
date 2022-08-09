aa51eddfbf704269ab64d6ad3c5afc173f59ac23
CHENGE8 <1911270452@qq.com>
2022-08-09 20:15:56 +0800

    test: fit database change

--- src/test/java/com/cool/movie/controller/BillControllerTest.java
+++ src/test/java/com/cool/movie/controller/BillControllerTest.java
@@ -1,6 +1,7 @@
 package com.cool.movie.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -23,11 +24,12 @@ public class BillControllerTest {
     @Test
     void should_return_BillResponse_when_perform_bills_api_given_orderId() throws Exception {
         // given
+
         Integer testId = 1;
         client.perform(MockMvcRequestBuilders.get("/bills/{id}", testId))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.cinemaName").value("cinema-1"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.movieName").value("movie-1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.movieName").value("my_movie"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(500))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.ticketCode").value("123456"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.partnerName").value("name-1"))
