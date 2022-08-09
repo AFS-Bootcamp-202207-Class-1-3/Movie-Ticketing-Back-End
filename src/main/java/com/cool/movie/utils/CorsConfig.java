872ce718f2930c2d6e71f0c33f0015df0ae72b5e
ToMyNorth <380816700@qq.com>
2022-08-09 11:31:41 +0800

    chore: implement the whole corsConfig

---
+++ src/main/java/com/cool/movie/utils/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.cool.movie.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+}
