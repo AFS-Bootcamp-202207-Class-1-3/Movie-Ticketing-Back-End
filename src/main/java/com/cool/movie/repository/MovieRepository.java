5ec561d95b6a247f7f7be2626997ac1dcd7e2007
IMCH005 <1628225498@qq.com>
2022-08-11 13:45:44 +0800

    feat: movieWithMessagePage of MovieRepository

--- src/main/java/com/cool/movie/repository/MovieRepository.java
+++ src/main/java/com/cool/movie/repository/MovieRepository.java
@@ -1,16 +1,21 @@
 package com.cool.movie.repository;
 
 
-import com.cool.movie.dto.movie.MovieWithMessagePage;
 import com.cool.movie.entity.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, String> {
-    Page<Movie> movieWithMessagePage(String sql,String countSql, Pageable pageable)
+    @Query(
+            value = "?1",
+            countQuery = "?2",
+            nativeQuery = true
+    )
+    Page<Movie> movieWithMessagePage(String sql, String countSql, Pageable pageable);
 }
 
