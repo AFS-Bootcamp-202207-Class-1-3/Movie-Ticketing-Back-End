8bf44f381a591b9827702fb47cfb987911ba7dc6
monsterleon <1115209655@qq.com>
2022-08-12 09:52:35 +0800

    change master database

--- src/main/resources/db/migration/V20220808194911__V1.13_insert_into_user_table.sql
+++ src/main/resources/db/migration/V20220808194911__V1.13_insert_into_user_table.sql
@@ -1,20 +1,20 @@
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('1','刘德华','JONE HUANG','http://img.keaiming.com/uploads/allimg/2019102118/lckfom0bc4m.jpg',21,'male','13642215498','湛江','My Name is JONE');
+VALUES ('1','刘德华','JONE HUANG','https://s1.328888.xyz/2022/08/12/65QwE.jpg',21,'male','13642215498','湛江','My Name is JONE');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('2','吴彦祖','Song Wang','https://www.woyaogexing.com/touxiang/nan/2022/1271721.html',22,'male','15812155448','武汉','My Name is 吴彦祖，I am a handsome boy');
+VALUES ('3','彭于晏','MARCUS MA','https://s1.328888.xyz/2022/08/12/658Ij.jpg',18,'male','13945224075','珠海','I am Marcus,I love coding');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('3','彭于晏','MARCUS MA','http://img.keaiming.com/uploads/allimg/2020021111/osowxhv4czn.jpeg',18,'male','13945224075','珠海','I am Marcus,I love coding');
+VALUES ('4','刘亦菲','Miky Zhao','https://s1.328888.xyz/2022/08/12/65t5m.jpg',16,'female','17548568698','珠海','Hello,Nice to see you');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('4','刘亦菲','Miky Zhao','http://img.keaiming.com/uploads/allimg/2016060813/5fgznrug3fe.jpg',16,'female','17548568698','珠海','Hello,Nice to see you');
+VALUES ('5','楚利略','Norris Chen','https://s1.328888.xyz/2022/08/12/65Urr.png',25,'male','13249555444','汕头','Yo,I love coding');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('5','楚利略','Norris Chen','http://img.keaiming.com/uploads/allimg/2016060813/d1jsgsrifvh.png',25,'male','13249555444','汕头','Yo,I love coding');
+VALUES ('6','周杰伦','GEORGE CHEN','https://s1.328888.xyz/2022/08/12/65g4I.jpg',23,'male','18523695415','珠海','Hi,I hope to enjoy movies');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('6','吴亦凡','GEORGE CHEN','http://img.keaiming.com/uploads/allimg/200929/3-2009291G153.jpg',23,'male','18523695415','珠海','Hi,I hope to enjoy movies');
+VALUES ('7','黄渤','KENDRICK CHEN','https://s1.328888.xyz/2022/08/12/65zvk.jpg',18,'male','12348756894','珠海','Happy every day in my life');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('7','黄渤','KENDRICK CHEN','http://img.keaiming.com/uploads/allimg/2019012012/ipqretgtxhg.jpg',18,'male','12348756894','珠海','Happy every day in my life');
+VALUES ('8','苏菲','Liz Li','https://s1.328888.xyz/2022/08/12/65yy7.jpg',18,'female','13985447125','珠海','Hi,I am Liz');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('8','苏菲','Liz Li','http://www.keaiming.com/touxiang/215515.html',18,'female','13985447125','珠海','Hi,I am Liz');
+VALUES ('9','林芝','Victor','https://s1.328888.xyz/2022/08/12/65Mg6.jpg',24,'female','14832569874','广东','I love watching movies');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('9','林芝','Victor','http://img.keaiming.com/uploads/allimg/2019032215/rpitcwvrw2o.jpg',24,'female','14832569874','广东','I love watching movies');
+VALUES ('10','林俊杰','Laughing Wu','https://s1.328888.xyz/2022/08/12/65PnX.jpg',20,'male','19847256854','珠海','Nice to see you again');
 INSERT INTO public.customer (id,real_name,nick_name,avatar_url,age,gender,phone_number,city,introduction)
-VALUES ('10','林俊杰','Laughing Wu','http://img.keaiming.com/uploads/allimg/2020090113/4dked1smvmu.jpg',20,'male','19847256854','珠海','Nice to see you again');
+VALUES ('2','吴彦祖','Song Wang','https://s1.328888.xyz/2022/08/12/65SgJ.jpg',22,'male','15812155448','武汉','My Name is 吴彦祖，I am a handsome boy')
